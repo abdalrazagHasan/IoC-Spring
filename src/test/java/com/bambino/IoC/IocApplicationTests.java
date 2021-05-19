728c1dbee0d30100393fad6375b52491c7c21eed
abdalrazaghassan <abdalrazag963@gmail.com>
2021-05-19 22:28:00 +0300

    add life cycle methods ( callback methods() )

--- src/test/java/com/bambino/IoC/IocApplicationTests.java
+++ src/test/java/com/bambino/IoC/IocApplicationTests.java
@@ -1,13 +1,18 @@
 package com.bambino.IoC;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class IocApplicationTests {
 
     @Test
     void contextLoads() {
+
+
     }
 
 }
